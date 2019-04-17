c7ed0d8acb0825a2988e3a152f92d57b00505884
Gaurav Joshi <gsj38@msstate.edu>
2019-04-16 22:52:12 -0500

    Checks and removes elements from cpp vectors

--- Arrays/27.cpp
+++ Arrays/27.cpp
@@ -4,6 +4,7 @@ public:
     {
         if(nums.size() == 0) return 0;
         
+        //calculates distance from start to end of the array while checking deletions
         return distance(nums.begin(), remove(nums.begin(), nums.end(), val));
     }
         
