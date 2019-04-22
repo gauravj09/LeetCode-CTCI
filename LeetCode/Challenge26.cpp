d8e4f69077a63a90f1df7b4df6e4fb893ac8cacd
Gaurav Joshi <gsj38@msstate.edu>
2019-04-22 14:51:04 -0500

    Update Challenge26.cpp

--- LeetCode/Challenge26.cpp
+++ LeetCode/Challenge26.cpp
@@ -1,1 +1,21 @@
-#Test
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int size = nums.size();
+        int i = 0;
+        
+        if(size == 0) return 0;
+        
+        for(int j = 0; j<size; j++)
+        {
+            if(nums[j] != nums[i])
+            {
+                i++;
+                nums[i] = nums[j];
+            }
+                
+        }
+        
+        return i+1;
+        
+    }
